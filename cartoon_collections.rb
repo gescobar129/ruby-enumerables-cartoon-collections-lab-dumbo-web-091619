d6b1916ef2a48b8ec821f324accbbd5d015779a0
Gaida M Escobar <gescobar129@gmail.com>
2019-09-10 17:25:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,8 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.each do |n|
-    if n.length > 4
+  array.any? do |n|
+    n.length > 4
 end
 
 def find_the_cheese# code an argument here
